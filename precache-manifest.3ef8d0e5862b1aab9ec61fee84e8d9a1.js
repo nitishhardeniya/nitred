5e379798e9f026da5661a13a5f62210fda932664
nitish <nitish.hardeniya@happay.in>
2019-06-12 13:01:28 +0530

    Updates

---
+++ precache-manifest.3ef8d0e5862b1aab9ec61fee84e8d9a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7193d0a7671f27b6e515fba01e426285",
+    "url": "/nitred/index.html"
+  },
+  {
+    "revision": "2754b682ab8844769607",
+    "url": "/nitred/static/css/main.f473bccb.chunk.css"
+  },
+  {
+    "revision": "671e6ede53bc82ee0fb5",
+    "url": "/nitred/static/js/2.195a4d3f.chunk.js"
+  },
+  {
+    "revision": "2754b682ab8844769607",
+    "url": "/nitred/static/js/main.b2e2b67a.chunk.js"
+  },
+  {
+    "revision": "f1681cbfb4c30042e95e",
+    "url": "/nitred/static/js/runtime~main.a64f27df.js"
+  }
+]);
